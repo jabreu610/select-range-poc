98ae63834348576dca8715e5e6a69d36af5aff3e
Jordan Abreu <jabreu610@gmail.com>
2025-04-25 17:54:43 -0400

    Implement select poc

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,95 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useRef, useState, forwardRef } from "react";
+import "./App.css";
+
+type FloatingMenuProps = {
+  x: number;
+  y: number;
+  show: boolean;
+  message: string;
+};
+
+const FloatingMenu = forwardRef<HTMLDivElement, FloatingMenuProps>(
+  ({ x, y, show, message }, ref) => {
+    return (
+      <div
+        ref={ref}
+        style={{
+          display: show ? "block" : "none",
+          position: "absolute",
+          top: y,
+          left: x,
+          backgroundColor: "rgba(245, 245, 245, 0.6)",
+          backdropFilter: "blur(5px)", 
+          border: "1px solid rgba(0, 0, 0, 1)", 
+          padding: "10px",
+          color: "rgba(0, 0, 0, 1)", 
+          zIndex: 1000,
+        }}
+      >
+        <pre style={{ textAlign: "left", margin: 0, fontWeight: 'bold' }}>{message}</pre>
+      </div>
+    );
+  }
+);
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [value, setValue] = useState("This is a test value");
+  const [showMenu, setShowMenu] = useState(false);
+  const mousePosRef = useRef({ x: 0, y: 0 });
+  const menuRef = useRef<HTMLDivElement>(null);
+  const [message, setMessage] = useState("");
+
+  const handleSelect = (e: React.SyntheticEvent<HTMLTextAreaElement>) => {
+    const target = e.target as HTMLTextAreaElement;
+    const start = target.selectionStart;
+    const end = target.selectionEnd;
+    const selectedText = target.value.substring(start, end);
+    setMessage(`Selected text: ${selectedText}
+Start: ${start}
+End: ${end}`);
+    setShowMenu(true);
+  };
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (event.target instanceof HTMLElement) {
+        if (
+          !event.target.closest("textarea") &&
+          !menuRef.current?.contains(event.target)
+        ) {
+          setShowMenu(false);
+        }
+      }
+    };
+
+    document.addEventListener("click", handleClickOutside);
+    return () => {
+      document.removeEventListener("click", handleClickOutside);
+    };
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <textarea
+        onSelect={handleSelect}
+        onMouseUp={(e) =>
+          (mousePosRef.current = { x: e.clientX, y: e.clientY })
+        }
+        style={{ fontSize: "16px" }}
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        rows={3}
+        cols={80}
+      />
+      <FloatingMenu
+        show={showMenu}
+        ref={menuRef}
+        message={message}
+        x={mousePosRef.current.x}
+        y={mousePosRef.current.y}
+      />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
